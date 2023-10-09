8e55cc02850095a39c8633cd44a002424e946afa
K39i <K39i@K39I>
2023-10-09 22:25:17 +0800

    Add project files.

---
+++ LOCKER/loginForm.cpp
@@ -0,0 +1,13 @@
+#include "loginForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	LOCKER::loginForm form;
+	Application::Run(% form);
+}
