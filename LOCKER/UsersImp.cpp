44cd2daf09b1ef6ecf44d57ae0c2bc3b43956605
immaNomad <ortiz.markdaniel09@gmail.com>
2023-10-27 15:09:35 +0800

    added title and desc

--- LOCKER/UsersImp.cpp
+++ LOCKER/UsersImp.cpp
@@ -1,5 +1,6 @@
 #include "Users.h"
 #include <filesystem>
+#include "json.hpp"
 
 namespace fs = std::filesystem;
 
@@ -35,6 +36,12 @@ void addCredentials(System::String^ username, System::String^ password) {
 	if (!fs::exists(folderName)) {
 		fs::create_directories(folderName);
 	}
+
+	nlohmann::ordered_json emptyJson;
+	emptyJson["images"] = {};
+	std::ofstream outfile(fs::current_path().string() + "\\UserFolders\\" + newUser.username + "\\" + newUser.username + ".json");
+	outfile << std::setw(4) << emptyJson;
+	outfile.close();
 
 	// Create numberOfEntries.txt file
 	std::string entriesFile = folderName + "\\numberOfEntries.txt";
--- LOCKER/homeForm.h
+++ LOCKER/homeForm.h
@@ -39,10 +39,12 @@ namespace LOCKER {
 	private: System::Windows::Forms::Button^ uploadButton;
 	private: System::Windows::Forms::Label^ greeting;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
+
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
 
 
 	private:
@@ -58,10 +60,10 @@ namespace LOCKER {
 			this->uploadButton = (gcnew System::Windows::Forms::Button());
 			this->greeting = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->headerImage))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->logoutButton))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
@@ -121,25 +123,6 @@ namespace LOCKER {
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Click += gcnew System::EventHandler(this, &homeForm::pictureBox1_Click);
 			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(287, 575);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(67, 25);
-			this->label1->TabIndex = 6;
-			this->label1->Text = L"TITLE";
-			this->label1->Click += gcnew System::EventHandler(this, &homeForm::label1_Click);
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(279, 618);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(75, 25);
-			this->label2->TabIndex = 7;
-			this->label2->Text = L"caption";
-			// 
 			// button1
 			// 
 			this->button1->Location = System::Drawing::Point(130, 620);
@@ -148,7 +131,7 @@ namespace LOCKER {
 			this->button1->TabIndex = 8;
 			this->button1->Text = L"previous";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &homeForm::button1_Click_1);
+			this->button1->Click += gcnew System::EventHandler(this, &homeForm::button1_Click);
 			// 
 			// button2
 			// 
@@ -160,16 +143,36 @@ namespace LOCKER {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &homeForm::button2_Click);
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(325, 592);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(64, 25);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"label1";
+			this->label1->Click += gcnew System::EventHandler(this, &homeForm::label1_Click_1);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(315, 646);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(64, 25);
+			this->label2->TabIndex = 11;
+			this->label2->Text = L"label2";
+			this->label2->Click += gcnew System::EventHandler(this, &homeForm::label2_Click);
+			// 
 			// homeForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(248)), static_cast<System::Int32>(static_cast<System::Byte>(244)),
 				static_cast<System::Int32>(static_cast<System::Byte>(235)));
 			this->ClientSize = System::Drawing::Size(624, 703);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->greeting);
 			this->Controls->Add(this->uploadButton);
@@ -190,7 +193,7 @@ namespace LOCKER {
 		}
 #pragma endregion
 	public: bool signOff = false;
-		  int currentPicture = 1;
+		  int currentPicture = 0;
 	private: System::Void logoutButton_Click(System::Object^ sender, System::EventArgs^ e) {
 		signOff = true;
 		this->Close();
@@ -203,8 +206,7 @@ namespace LOCKER {
 		openUpload = true;
 		this->Close();
 	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
+
 	public:	String^ jsonFilePath;
 	private: System::Void UpdateImage() {
 		msclr::interop::marshal_context context;
@@ -212,26 +214,41 @@ namespace LOCKER {
 		nlohmann::ordered_json imageJson;
 		std::ifstream inJson(jsonFilePathString);
 		inJson >> imageJson;
-		String^ imagePath = gcnew String(imageJson["images"][currentPicture]["imgpath"].get<std::string>().c_str());
 
-		System::String^ currUserPathString = gcnew String(jsonFilePathString.c_str());
-		if (System::IO::File::Exists(imagePath)) {
-			pictureBox1->ImageLocation = imagePath;
+		int currentPicture = 0; // Replace with the index of the desired image.
+
+		// Get image path, description, and title
+		std::string imagePath = imageJson["images"][currentPicture]["imgpath"].get<std::string>();
+		std::string imgDesc = imageJson["images"][currentPicture]["img_desc"].get<std::string>();
+		std::string imgTitle = imageJson["images"][currentPicture]["img_title"].get<std::string>();
+
+		// Update PictureBox with the image
+		String^ imagePathStr = gcnew String(imagePath.c_str());
+		if (System::IO::File::Exists(imagePathStr)) {
+			pictureBox1->ImageLocation = imagePathStr;
 		}
+
+		// Display image description and title
+		label1->Text = gcnew String(imgDesc.c_str());
+		label2->Text = gcnew String(imgTitle.c_str());
+
 		pictureBox1->BringToFront();
+	}
+	private: System::Void label1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 
 private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
+
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 		currentPicture++;
 		UpdateImage();
-		next = true;
 	}
-	private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		currentPicture--;
 		UpdateImage();
-		previous = true;
 	}
 };
 }
